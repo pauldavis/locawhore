9e4d370e2b46638277f6955148f2267184573818
Paul Davis <paul@watermarktech.com>
2012-05-26 09:28:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locawhore::Application.config.secret_token = '23d6316ce5d361feb2ec085e4f7c5467aa6b97054472b97311b44c8ea5db6379c6190e970f91faf22e79da6383f093ba20a97186341e7a37c835e7ed70c59c72'
